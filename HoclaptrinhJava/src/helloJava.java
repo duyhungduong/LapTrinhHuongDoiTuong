f4c0489b5ab3d898540cf0d6949bfb68b261b4c0
duyhungduong <duyhungduong10@gmail.com>
2024-10-15 13:53:05 +0700

    first commmit

---
+++ HoclaptrinhJava/src/helloJava.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class helloJava {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hoc Java");
+        System.out.println("Dương Duy Hưng B2103500");
+        Scanner sc = new Scanner(System.in);
+        String s= sc.nextLine();
+        System.out.println("----------");
+        System.out.println(s);
+    }
+}
---
+++ javabaitap/vidu1/src/vidu1/HelloXinchao.java
@@ -0,0 +1,7 @@
+package vidu1;
+
+public class HelloXinchao {
+	public static void main(String[] args) {
+		System.out.println("Xin chào các bạn!");
+	}
+}
